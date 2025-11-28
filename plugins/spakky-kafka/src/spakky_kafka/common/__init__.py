991a7f8d1175e8de80dc80bc14e918098f4eb74e
Spakky <sejong418@icloud.com>
2025-11-28 21:54:05 +0900

    fix(kafka): 잘못된 코드 제거

--- plugins/spakky-kafka/src/spakky_kafka/common/__init__.py
+++ plugins/spakky-kafka/src/spakky_kafka/common/__init__.py
@@ -1,2 +1,5 @@
-def main() -> None:
-    print("Hello from spakky-kafka!")
+"""Common utilities for Kafka integration."""
+
+from spakky_kafka.common.config import AutoOffsetResetType, KafkaConnectionConfig
+
+__all__ = ["AutoOffsetResetType", "KafkaConnectionConfig"]
--- plugins/spakky-kafka/src/spakky_kafka/event/__init__.py
+++ plugins/spakky-kafka/src/spakky_kafka/event/__init__.py
@@ -1,2 +1,11 @@
-def main() -> None:
-    print("Hello from spakky-kafka!")
+"""Event publishing and consuming for Kafka."""
+
+from spakky_kafka.event.consumer import AsyncKafkaEventConsumer, KafkaEventConsumer
+from spakky_kafka.event.publisher import AsyncKafkaEventPublisher, KafkaEventPublisher
+
+__all__ = [
+    "AsyncKafkaEventConsumer",
+    "AsyncKafkaEventPublisher",
+    "KafkaEventConsumer",
+    "KafkaEventPublisher",
+]
