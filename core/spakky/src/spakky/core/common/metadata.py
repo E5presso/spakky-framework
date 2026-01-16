728dc63e048e949733d826e8a3daef4a3e70f57f
Spakky <sejong418@icloud.com>
2026-01-16 15:25:13 +0900

    fix: 타입 추론 오류 수정

--- core/spakky/src/spakky/core/common/metadata.py
+++ core/spakky/src/spakky/core/common/metadata.py
@@ -7,7 +7,7 @@ from abc import ABC
 from dataclasses import dataclass
 from typing import Annotated, cast, get_args
 
-from spakky.core.common.types import ClassT
+from spakky.core.common.types import AnyT
 
 
 @dataclass
@@ -18,18 +18,18 @@ class AbstractMetadata(ABC):
 
 
 def get_metadata(
-    annotated: Annotated[ClassT, ...],
-) -> tuple[ClassT, list[AbstractMetadata]]:
+    annotated: Annotated[AnyT, ...],
+) -> tuple[AnyT, list[AbstractMetadata]]:
     """Extract the type and metadata from an Annotated type hint.
 
     Args:
         annotated: An Annotated type hint containing a type and metadata.
 
     Returns:
-        tuple[ClassT, list[Metadata]]: A tuple of (type, metadata_list) where metadata_list
-            contains only instances of Metadata subclasses.
+        tuple[Any, list[AbstractMetadata]]: A tuple of (type, metadata_list) where metadata_list
+            contains only instances of AbstractMetadata subclasses.
     """
     metadata = get_args(annotated)
-    return cast(ClassT, metadata[0]), [
+    return cast(AnyT, metadata[0]), [
         data for data in metadata[1:] if isinstance(data, AbstractMetadata)
     ]
